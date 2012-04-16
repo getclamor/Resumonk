d5780d7a9d13ca6928b066eb59513e1ffabb78b0
Bharani M <bharani91@gmail.com>
2012-04-17 00:57:05 +0530

    gemfile changes

--- vendor/plugins/wicked_pdf/lib/wicked_pdf_helper.rb
+++ vendor/plugins/wicked_pdf/lib/wicked_pdf_helper.rb
@@ -20,7 +20,7 @@ module WickedPdfHelper
   module Assets
     def wicked_pdf_stylesheet_link_tag(*sources)
       sources.collect { |source|
-        "<style type='text/css'>#{Rails.application.assets.find_asset(source+".css")}</style>"
+        "<style type='text/css'>#{Rails.root.join('assets', source+".css")}</style>"
       }.join("\n").html_safe
     end
 
