913650206f0c7423c3a9add5e621d0a05879bf3d
Bharani M <bharani91@gmail.com>
2012-04-17 02:05:43 +0530

    minor changes

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,5 +2,5 @@ WickedPdf.config = {
   #:wkhtmltopdf => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s,
   #:layout => "pdf.html",
   #:exe_path => '/usr/local/bin/wkhtmltopdf'
-  :exe_path => "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  :wkhtmltopdf => "#{Rails.root}/bin/wkhtmltopdf-amd64"
 }
