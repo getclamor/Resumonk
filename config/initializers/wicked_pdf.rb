3cdf4f5c71f8e857fb1f3b9ef08144de38e39331
Bharani M <bharani91@gmail.com>
2012-04-17 02:09:42 +0530

    filestructure changed

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
 WickedPdf.config = {
   #:wkhtmltopdf => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s,
   #:layout => "pdf.html",
-  #:exe_path => '/usr/local/bin/wkhtmltopdf'
-  :wkhtmltopdf => "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  # :exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => "#{Rails.root}/bin/wkhtmltopdf-amd64"
 }
